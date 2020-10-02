3c5458b3a762be1c7ed286a212a06ef110b315f6
Eduardo Barron <st.buendia@gmail.com>
2020-10-01 23:07:26 -0500

    feature: Booleanos y condiciones

---
+++ 01. Introduccion Dart/03. BooleanosCondiciones.dart
@@ -0,0 +1,15 @@
+void main() {
+  bool activado = true;
+  print(activado);
+  if(activado == true){
+    print('El motor esta encendido');
+  }else{
+    print('Esta apagado');
+  }
+  
+  if(!activado){
+    print('El motor esta encendido');
+  }else{
+    print('Esta apagado');
+  }
+}
