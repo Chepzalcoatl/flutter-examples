c02fdff38732f2adc33027f5090355ac63df235f
Eduardo Barron <st.buendia@gmail.com>
2020-10-01 22:57:48 -0500

    feature: numero y strings

---
+++ 01. Introduccion Dart/02. NumeroStrings.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Numeros
+  int empleado = 10;
+  double pi = 3.141592;
+  double numero = 1.0;
+
+  print('$empleado - $pi - $numero');
+
+  // Strings - cadenas de caracteres
+  String nombre = 'To"ny';
+
+  print(nombre);
+  print(nombre[0]);
+  print(nombre[nombre.length - 1]);
+}
+
